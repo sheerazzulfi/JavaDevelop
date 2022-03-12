be976353ec7129caebd26a84ea611bee5bd09bbc
Sheeraz Zulfi <Sheerazzulfi123@gmail.com>
2022-03-12 16:43:37 +0530

    New files in branch2

---
+++ Chiran.java
@@ -0,0 +1,20 @@
+class Chiran
+{
+	public static boolean test(String a,String b)
+	{
+		boolean res= false;
+		for (int i=0;i<b.length() ;i++ ) 
+		{
+			res=a.contains(b.charAt(i));
+			
+		}
+		return res;
+	}
+	public static void main(String[] args) 
+	{
+		String a="chiranjeevi";
+		String b="rain";
+		System.out.println(test(a,b));
+		
+	}
+}
---
+++ ComlErr.java
@@ -0,0 +1,11 @@
+class ComlErr
+{
+	public static void main(String[] args) 
+	{
+		int a=10
+		System.out.println(a);	
+		System.out.println("P.E");
+	}
+}
+
+
